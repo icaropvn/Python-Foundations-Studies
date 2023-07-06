0429ba9541254b24a94e7749d75972a44c1f3d87
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-06 12:45:37 -0300

    ex 60 while update

--- ExerciciosPython/Exercício 060 - Fatorial (while).py
+++ ExerciciosPython/Exercício 060 - Fatorial (while).py
@@ -6,19 +6,22 @@ Algoritmo para ler um número
 e mostrar seu fatorial.
 '''
 
+# from math import factorial
+# A biblioteca math possui um método para o cálculo de fatorial
+
 cian = '\033[3;36m'
 yellow = '\033[3;33m'
 end = '\033[m'
 
-num = int(input('Insira um número para o fatorial: '))
+num = int(input('Insira um número para calcular o fatorial: '))
 
 print(f'{cian}{num}!{end} = ', end='')
 
 fat = 1
-while num != 0:
-    fat = fat * num
-    if num != 1:
+while num > 0:
+    fat *= num
+    if num > 1:
         print(f'{num} * ', end='')
     num -= 1
 
-print(f'1 = {yellow}{fat}{end}', end='')
+print(f'{num+1} = {yellow}{fat}{end}\n', end='')
