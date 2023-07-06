05e5e4649afa190f9de4cdd3731a87bb7019cb9c
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-06 11:44:37 -0300

    ex 57 update

--- ExerciciosPython/Exercício 057 - Validação de Dados.py
+++ ExerciciosPython/Exercício 057 - Validação de Dados.py
@@ -7,14 +7,17 @@ como resposta 'M', 'F' ou 'OUTRO'.
 Caso a resposta seja inválida, ler novamente.
 '''
 
-sexo = ''
+red = '\033[31m'
+ita = '\033[3m'
+end = '\033[m'
 
+sexo = str(input('Insira seu sexo (M, F ou OUTRO): ')).upper().strip()
 while sexo != 'M' and sexo != 'F' and sexo != 'OUTRO':
-    sexo = str(input('Insira seu sexo (M, F ou OUTRO): ')).upper()
+    sexo = str(input(f'{red}Dado inválido.{end} Insira seu sexo: ')).upper().strip()
 
 if sexo == 'M':
-    print('\nBem-vindo!')
+    print(f'\n{ita}Bem-vindo!{end}')
 elif sexo == 'F':
-    print('\nBem-vinda!')
+    print(f'\n{ita}Bem-vinda!{end}')
 else:
-    print('\nBem-vindo(a)!')
+    print(f'\n{ita}Bem-vindo(a)!{end}')
