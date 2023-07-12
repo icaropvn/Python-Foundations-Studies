011378ff4cf9001cd3c8522084ff9fd726ff0656
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-12 18:19:31 -0300

    ex 70 add

---
+++ ExerciciosPython/Exercício 070 - Estatística de Produtos.py
@@ -0,0 +1,62 @@
+'''
+Exercício 070
+"Estatística de Produtos"
+
+Algoritmo para ler o nome e o preço de
+indeterminados produtos. O programa deve perguntar
+se o usuário deseja continuar. E no final deve
+mostrar:
+
+- Qual é o total a ser pago
+- Quantos produtos custam mais de R$1000
+- Qual é o nome do produto mais barato
+'''
+
+red = '\033[31m'
+ita = '\033[3m'
+end = '\033[m'
+
+shop_name = 'SIRSO\'S COMPANY'
+print(14*'---' + f'\n{shop_name:^42}\n' + 14*'---')
+
+tot = 0
+maiormil = 0
+barato = 0
+barato_name = ''
+cont = 1
+stop = 0
+while True:
+    name = str(input(f'Nome do produto {cont}: '))
+    price = float(input(f'Preço do produto {cont}: R$'))
+    print(14*'---')
+
+    if cont == 1:
+        barato = price
+
+    tot += price
+
+    if price > 1000:
+        maiormil += 1
+    if price < barato:
+        barato = price
+        barato_name = name
+
+    while True:
+        answer = str(input(f'{ita}Deseja cadastrar mais produtos? [S/N]{end} ')).strip().upper()
+        if answer == 'S':
+            cont += 1
+            print(14 * '---')
+            break
+        elif answer == 'N':
+            stop = 1
+            print(14 * '---')
+            break
+        else:
+            print(f'{red}Resposta inválida.{end}\n' + 14*'---')
+
+    if stop == 1:
+        break
+
+print(f'Total a pagar: {ita}R${tot}{end}\n'
+      f'Produtos que custam mais de R$1000: {ita}{maiormil}{end}\n'
+      f'Produto mais barato: {ita}{barato_name} (R${barato}){end}')
