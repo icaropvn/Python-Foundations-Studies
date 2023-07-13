97420caba31e7037b82d26349ea9cf25406af6d0
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-13 18:23:49 -0300

    ex 70 update

--- ExerciciosPython/Exercício 070 - Estatística de Produtos.py
+++ ExerciciosPython/Exercício 070 - Estatística de Produtos.py
@@ -19,16 +19,14 @@ end = '\033[m'
 shop_name = 'SIRSO\'S COMPANY'
 print(14*'---' + f'\n{shop_name:^42}\n' + 14*'---')
 
-tot = 0
-maiormil = 0
-barato = 0
+tot = maiormil = barato = 0
 cont = 1
 while True:
     name = str(input(f'Nome do produto {cont}: '))
     price = float(input(f'Preço do produto {cont}: R$'))
     print(14*'---')
 
-    if cont == 1:
+    if cont == 1 or price < barato:
         barato = price
         barato_name = name
 
@@ -36,9 +34,6 @@ while True:
 
     if price > 1000:
         maiormil += 1
-    if price < barato:
-        barato = price
-        barato_name = name
 
     while True:
         answer = str(input(f'{ita}Deseja cadastrar mais produtos? [S/N]{end} ')).strip().upper()
