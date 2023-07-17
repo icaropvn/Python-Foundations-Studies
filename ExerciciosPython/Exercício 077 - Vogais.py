9925cf07f6d4e0e89f8328de50e6c7e628a6119a
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-17 18:43:24 -0300

    ex 77 add

---
+++ ExerciciosPython/Exercício 077 - Vogais.py
@@ -0,0 +1,11 @@
+'''
+Exercício 077
+"Vogais"
+
+Algoritmo que contenha uma tupla com várias palavras (sem acentos).
+Mostrar para cada palavra quais são suas vogais.
+'''
+
+words = ('magnetismo', 'salgado', 'castor', 'rico', 'declarar', 'vampiro'
+         'clube', 'mascote', 'vergonha', 'cardeal')
+
