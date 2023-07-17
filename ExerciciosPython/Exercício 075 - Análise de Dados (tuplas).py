6b6152dcdfd69ceb88e9357d28036c4952a0c44f
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-17 15:14:57 -0300

    ex 75 update

---
+++ ExerciciosPython/Exercício 075 - Análise de Dados (tuplas).py
@@ -0,0 +1,12 @@
+'''
+Exercício 075
+"Análise de Dados (tuplas)"
+
+Algoritmo para ler 4 valores e colocá-los
+numa tupla. Ao final, mostrar:
+
+- Quantas vezes o número 9 foi digitado
+- Em qual posição foi digitado todos os valores 3
+- Quais foram os números pares digitados
+'''
+
