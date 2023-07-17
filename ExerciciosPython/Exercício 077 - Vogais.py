b41b52aa999f813b82a8e74f9040d7f60483d45a
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-17 19:16:40 -0300

    ex 77 update

--- ExerciciosPython/Exercício 077 - Vogais.py
+++ ExerciciosPython/Exercício 077 - Vogais.py
@@ -6,6 +6,32 @@ Algoritmo que contenha uma tupla com várias palavras (sem acentos).
 Mostrar para cada palavra quais são suas vogais.
 '''
 
-words = ('magnetismo', 'salgado', 'castor', 'rico', 'declarar', 'vampiro'
+words = ('magnetismo', 'salgado', 'castor', 'rico', 'declarar', 'vampiro',
          'clube', 'mascote', 'vergonha', 'cardeal')
 
+flagA = 0
+flagE = 0
+flagI = 0
+flagO = 0
+flagU = 0
+
+for word in words:
+    print(f'A palavra {word.upper()} possui as vogais: ', end='')
+    for letter in word:
+        if letter == 'a' and flagA == 0:
+            print(letter, end=' ')
+            flagA = 1
+        elif letter == 'e' and flagE == 0:
+            print(letter, end=' ')
+            flagE = 1
+        elif letter == 'i' and flagI == 0:
+            print(letter, end=' ')
+            flagI = 1
+        elif letter == 'o' and flagO == 0:
+            print(letter, end=' ')
+            flagO = 1
+        elif letter == 'u' and flagU == 0:
+            print(letter, end=' ')
+            flagU = 1
+    flagA = flagE = flagI = flagO = flagU = 0
+    print('\n', end='')
