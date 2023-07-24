633fb347210002192ba22e10fea62d0941b96132
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-24 11:41:32 -0300

    ex 72 update

--- ExerciciosPython/Exercício 072 - Contagem por Extenso.py
+++ ExerciciosPython/Exercício 072 - Contagem por Extenso.py
@@ -8,6 +8,8 @@ escritos por extenso.
 Ler um número entre 0 e 20 do usuário e mostrá-lo por extenso.
 '''
 
+from time import sleep
+
 red = '\033[3;31m'
 ita = '\033[3m'
 end = '\033[m'
@@ -16,11 +18,24 @@ end = '\033[m'
 nums = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
         'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
+stop = 0
 while True:
     user = int(input('Insira um número entre 0 e 20: '))
     if user < 0 or user > 20:
         print(f'{red}Resposta inválida.{end}')
     else:
-        break
+        print(f'O número {user} por extenso é: {ita}{nums[user]}{end}\n')
+        sleep(1.5)
+        while True:
+            answer = str(input(f'{ita}Gostaria de inserir outro número? [S/N]{end}\n')).strip().lower()
+            if answer == 's' or answer == 'sim':
+                break
+            elif answer == 'n' or answer == 'nao' or answer == 'não':
+                stop = 1
+                break
+            else:
+                print(f'{red}Resposta inválida.{end}')
+        if stop == 1:
+            break
 
-print(f'O número {user} por extenso é: {ita}{nums[user]}{end}')
+print(f'\n{ita}Certo! Até a próxima!{end}')
