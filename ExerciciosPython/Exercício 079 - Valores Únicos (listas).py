04a20c1cc8ecd4035668e1e4ad231db7ffb3a526
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-26 15:30:29 -0300

    ex 79 update

--- ExerciciosPython/Exercício 079 - Valores Únicos (listas).py
+++ ExerciciosPython/Exercício 079 - Valores Únicos (listas).py
@@ -10,7 +10,35 @@ No final, serão exibidos todos os valores únicos da lista
 em ordem CRESCENTE
 '''
 
+red = '\033[3;31m'
+ita = '\033[3m'
+end = '\033[m'
+
 nums = list()
 
+stop = 0
 while True:
-    nums.append(int(input('Digite um valor: ')))
+    num = int(input('> Digite um valor: '))
+    if num not in nums:
+        nums.append(num)
+    else:
+        print(f'{red}O valor digitado é uma duplicata.{end}\n')
+
+    while True:
+        answer = str(input(f'{ita}Gostaria de inserir mais valores? [S/N]{end}\n')).strip().lower()
+        if answer == 'sim' or answer == 's':
+            print('\n', end='')
+            break
+        elif answer == 'não' or answer == 'nao' or answer == 'n':
+            stop = 1
+            break
+        else:
+            print(f'{red}Resposta inválida.{end}\n')
+
+    if stop == 1:
+        break
+
+nums.sort()
+print(20 * '=-=' + '\n'
+      f'{ita}Lista dos valores inseridos:{end}\n'
+      f'{nums}')
