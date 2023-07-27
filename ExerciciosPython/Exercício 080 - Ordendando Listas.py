4859b462fba99f45d7e8e6589d6f0c13aafc273b
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-07-27 12:40:42 -0300

    ex 80 update

--- ExerciciosPython/Exercício 080 - Ordendando Listas.py
+++ ExerciciosPython/Exercício 080 - Ordendando Listas.py
@@ -10,19 +10,25 @@ a lista seja ordenada de forma crescente.
 Usar o comando .sort() é proibido.
 '''
 
+green = '\033[3;32m'
+end = '\033[m'
+
 nums = list()
 
 for i in range(0, 5):
     if i == 0:
         nums.append(int(input(f'Digite o 1º valor: ')))
+        print(f'{green}Primeiro valor adicionado à lista.{end}')
     else:
         num = int(input(f'Digite o {i+1}º valor: '))
         for j in range(0, len(nums)):
             if num < nums[j]:
                 nums.insert(j, num)
+                print(f'{green}Valor adicionado na posição {j} da lista.{end}')
                 break
             if j == len(nums) - 1:
                 nums.append(num)
+                print(f'{green}Valor adicionado no FINAL da lista.{end}')
 
 print(20 * '==' + '\n'
       f'Lista dos valores em ordem crescente:\n'
